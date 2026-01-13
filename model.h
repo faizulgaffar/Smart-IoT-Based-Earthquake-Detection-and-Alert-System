ec8cd0d727a8d9dc393a43b149f94debdace217a
Shaik Faizul Gaffar <127026997+faizulgaffar@users.noreply.github.com>
2026-01-13 23:50:16 +0530

    Add model.h file

---
+++ model.h
@@ -0,0 +1,127 @@
+#pragma once
+#include <cstdarg>
+namespace Eloquent {
+    namespace ML {
+        namespace Port {
+            class RandomForest {
+                public:
+                    /**
+                    * Predict class for features vector
+                    */
+                    int predict(float *x) {
+                        uint8_t votes[2] = { 0 };
+                        // tree #1
+                        if (x[3] <= 0.6897600442171097) {
+                            votes[0] += 1;
+                        }
+
+                        else {
+                            votes[1] += 1;
+                        }
+
+                        // tree #2
+                        if (x[1] <= 0.5986815541982651) {
+                            votes[0] += 1;
+                        }
+
+                        else {
+                            votes[1] += 1;
+                        }
+
+                        // tree #3
+                        if (x[1] <= 0.5926387161016464) {
+                            votes[0] += 1;
+                        }
+
+                        else {
+                            votes[1] += 1;
+                        }
+
+                        // tree #4
+                        if (x[4] <= -0.6245704144239426) {
+                            votes[1] += 1;
+                        }
+
+                        else {
+                            if (x[5] <= 0.8272518366575241) {
+                                votes[0] += 1;
+                            }
+
+                            else {
+                                votes[1] += 1;
+                            }
+                        }
+
+                        // tree #5
+                        if (x[1] <= 0.5986815541982651) {
+                            votes[0] += 1;
+                        }
+
+                        else {
+                            votes[1] += 1;
+                        }
+
+                        // tree #6
+                        if (x[5] <= 0.8272518366575241) {
+                            votes[0] += 1;
+                        }
+
+                        else {
+                            votes[1] += 1;
+                        }
+
+                        // tree #7
+                        if (x[1] <= 0.6256855875253677) {
+                            votes[0] += 1;
+                        }
+
+                        else {
+                            votes[1] += 1;
+                        }
+
+                        // tree #8
+                        if (x[1] <= 0.5986815541982651) {
+                            votes[0] += 1;
+                        }
+
+                        else {
+                            votes[1] += 1;
+                        }
+
+                        // tree #9
+                        if (x[1] <= 0.6038428097963333) {
+                            votes[0] += 1;
+                        }
+
+                        else {
+                            votes[1] += 1;
+                        }
+
+                        // tree #10
+                        if (x[3] <= 0.6914738863706589) {
+                            votes[0] += 1;
+                        }
+
+                        else {
+                            votes[1] += 1;
+                        }
+
+                        // return argmax of votes
+                        uint8_t classIdx = 0;
+                        float maxVotes = votes[0];
+
+                        for (uint8_t i = 1; i < 2; i++) {
+                            if (votes[i] > maxVotes) {
+                                classIdx = i;
+                                maxVotes = votes[i];
+                            }
+                        }
+
+                        return classIdx;
+                    }
+
+                protected:
+                };
+            }
+        }
+    }
